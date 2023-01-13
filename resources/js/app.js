5a5464b0fa5ce7644587753464f189df219cb627
Hitech10 <christian.n.hiram.com>
2023-01-13 15:29:34 +0100

    dashbord

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
-import { createApp } from 'vue';
-import app from './components/app.vue';
-import router from './router';
 
-createApp(app).use(router).mount('#app');
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
