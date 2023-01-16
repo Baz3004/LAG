329a3b01bd625bb6e367679c864f99248c3bdd77
Plblanche <mercidinkelani@gmail.com>
2023-01-16 19:16:50 +0100

    apres arrangement Japhet

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+   <navBar/>
+   <Footer/>
+</template>
+<script>
+import navBar from './components/navBar.vue';
+import Footer from './components/Footer.vue'
+
+export default {
+    name :'app',
+    components : {
+        navBar,
+        Footer
+    }
+}
+
+</script>
